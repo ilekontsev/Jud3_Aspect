d99275f4239ef959e45283e881087f6972865f93
Ivan <ivan.lek123@gmail.com>
2022-12-06 19:28:09 +0300

    fixed errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameFieldComponent } from './game/game-field/game-field.component';
 import { GameFieldTreejsComponent } from './game/game-field-treejs/game-field-treejs.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './mainComponents/main/main.component';
+import { SubHeaderComponent } from './mainComponents/sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameFieldComponent,
-    GameFieldTreejsComponent
+    GameFieldTreejsComponent,
+    MainComponent,
+    SubHeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
